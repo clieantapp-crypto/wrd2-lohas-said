7ca84ce336c60657f22395e82d4482d78cc97ce8
alopopau-eng <alopopau@gmail.com>
2026-01-19 04:33:10 +0300

    aa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  // Custom rules
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
+  // Override default ignores of eslint-config-next
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
