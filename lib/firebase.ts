96efb3d170656fe2342ce4d1f8ff75cebb19f540
clieantapp-crypto <clieantapp@gmail.com>
2026-02-04 17:14:13 +0300

    Change Firebase configuration settings
    Updated Firebase configuration with new project details.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
- apiKey: "AIzaSyDKv0L5VjPP7oD12Cwk6zA15-GGBpY-tPs",
-  authDomain: "mmdm-6fb93.firebaseapp.com",
-  databaseURL: "https://mmdm-6fb93-default-rtdb.firebaseio.com",
-  projectId: "mmdm-6fb93",
-  storageBucket: "mmdm-6fb93.firebasestorage.app",
-  messagingSenderId: "657145391802",
-  appId: "1:657145391802:web:aa2ef55a0b9d41d9e17083",
-  measurementId: "G-8J5VLQMT5P"
+  apiKey: "AIzaSyCVuDP2-RaK509rdoZUGarOkdBb4CpT_f8",
+  authDomain: "neewtaminsss.firebaseapp.com",
+  databaseURL: "https://neewtaminsss-default-rtdb.firebaseio.com",
+  projectId: "neewtaminsss",
+  storageBucket: "neewtaminsss.firebasestorage.app",
+  messagingSenderId: "873192027415",
+  appId: "1:873192027415:web:724aafce2bcdbdb8e73352",
+  measurementId: "G-JB0KP1FZH4"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
